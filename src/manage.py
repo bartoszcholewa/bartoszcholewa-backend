702d378913aca0ab6757e664305f1351fa7635fb
bartosz <bartosz.cholewa@lightcode.eu>
2022-06-02 02:40:09 +0200

    django-dotenv configuration

--- src/manage.py
+++ src/manage.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python
+# pylint: skip-file
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
+
+import dotenv
 
 
 def main():
@@ -19,4 +22,5 @@ def main():
 
 
 if __name__ == '__main__':
+    dotenv.read_dotenv()
     main()
