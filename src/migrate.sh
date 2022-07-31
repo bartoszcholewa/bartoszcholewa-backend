d9ef26d0fed9b1c9d8a7a495f17c213ffda38317
bartosz <bartosz.cholewa@lightcode.eu>
2022-07-31 05:47:01 +0200

    kubernetes part 6 - actions deploy

--- src/migrate.sh
+++ src/migrate.sh
@@ -4,5 +4,4 @@ SUPERUSER_EMAIL=${DJANGO_SUPERUSER_EMAIL:-"root@root.pl"}
 # shellcheck disable=SC2164
 cd /app/
 
-/opt/venv/bin/python manage.py migrate --noinput
-/opt/venv/bin/python manage.py createsuperuser --email $SUPERUSER_EMAIL --noinput || true
+python manage.py migrate --noinput
