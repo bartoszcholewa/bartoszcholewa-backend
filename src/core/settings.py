d94323210f479c3ab6a874b92c6207c7edaaded8
bartosz <bartosz.cholewa@lightcode.eu>
2022-07-31 08:28:27 +0200

    test

--- src/core/settings.py
+++ src/core/settings.py
@@ -132,7 +132,7 @@ AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_STORAGE_BUCKET_NAME')
 
-if os.getenv('USE_S3') == 'TRUE':
+if os.environ.get('USE_S3') == 'True':
     AWS_DEFAULT_ACL = None
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
     AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
